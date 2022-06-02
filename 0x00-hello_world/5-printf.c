bd671fe84bd2cbced1fecfea00fb603ceec58b62
jo-nigue <yohaness567@gmail.com>
2022-06-02 13:25:49 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ *main - uses printf
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char *ptr="with proper grammar, but the outcome is a piece of art,";
+      printf("%s", ptr);
+      return (0);
+}
