7282a25ff3d6cbe003e4c1ed9d37b2fac48cef35
jo-nigue <yohaness567@gmail.com>
2022-06-02 13:34:55 -0700

    printf space added and betty applied again

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,7 +6,6 @@
  */
 int main(void)
 {
-char *ptr = "with proper grammar, but the outcome is a piece of art, ";
-printf("%s", ptr"\n");
+printf("with proper grammar, but the outcome is a piece of art, \n");
 return (0);
 }
