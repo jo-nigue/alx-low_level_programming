5cf990e77ce6ce13d9d0ae88cfc16a419c28efa2
jo-nigue <yohaness567@gmail.com>
2022-06-02 13:18:32 -0700

    betty applied

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
 
+/**
+ * main-string printing using puts
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-   char *s="\"Programming is like building a multilingual puzzle";
-	   puts(s);
-	return(0);
+char *s = "\"Programming is like building a multilingual puzzle";
+	   puts (s);
+	return (0);
 }
