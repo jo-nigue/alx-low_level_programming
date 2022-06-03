feca07c9dd825a61826f9d9e24b9aaecdf12f75b
jo-nigue <yohaness567@gmail.com>
2022-06-03 13:54:08 -0700

    last digit second trial

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,22 +2,21 @@
 #include<time.h>
 #include<stdio.h>
 /*
- * main-contains procedures to specify the last digit
+ * main - contains procedures to specify the last digit
  *
- * return 0 if succesful
- *
+ * Return: Always 0 if succesful
  */
 int main(void)
 {
 int n,lastD;
 srand(time(0));
-n=rand() - RAND_MAX / 2;
-lastD = n%10;
+n= rand() - RAND_MAX / 2;
+lastD = n % 10;
 if (lastD > 5)
-printf("Last digit of %d is %d and is greater than 5\n",n,lastD);
+printf("Last digit of %d is %d and is greater than 5\n", n, lastD);
 else if (lastD == 0)
-printf("Last digit of %d is %d and is 0\n",n,lastD);
+printf("Last digit of %d is %d and is 0\n", n, lastD);
 else if (lastD < 6)
-printf("Last digit of %d is %d and is less than 6 and not 0\n",n,lastD);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastD);
 return (0);
 }
