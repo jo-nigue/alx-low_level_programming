dde17834adae73e94935b2a3563b0a8603370fd7
jo-nigue <yohaness567@gmail.com>
2022-06-04 13:35:15 -0700

    hope its my final attempt

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,7 +4,7 @@
  *
  * Return: 0 if successful
  */
-int main (void)
+int main(void)
 {
 int num, num1;
 for (num = '0'; num <= '9'; num++)
@@ -15,8 +15,13 @@ if (num1 > num)
 {
 putchar (num);
 putchar (num1);
+if (num == '8' && num1 == '9')
+break;
+else
+{
 putchar (',');
 putchar (' ');
+}
 }
 else
 continue;
