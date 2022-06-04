7e0a2c040afeabe5254a6ebc1a0b127b47768ba6
jo-nigue <yohaness567@gmail.com>
2022-06-04 10:28:08 -0700

    0-9 printed

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - prints 1 upto 0
+ *
+ * Return: 0 if success
+ */
+int main(void)
+{
+int number;
+for (number = 0; number < 10; number++)
+printf("%d",number);
+putchar('\n');
+return (0);
+}
