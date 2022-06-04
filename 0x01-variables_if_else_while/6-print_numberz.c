fd90fcfd0340b1d2b3668d948e676d845ef1e817
jo-nigue <yohaness567@gmail.com>
2022-06-04 10:42:12 -0700

    0 to 9 without using char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - prints 0 upto 9
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+int num;
+for (num = 48; num <= 57; num++)
+{
+putchar(num);
+}
+putchar('\n');
+return (0);
+}
