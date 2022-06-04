802eceb47147e179f43651788be6930cd770d1bf
jo-nigue <yohaness567@gmail.com>
2022-06-04 12:04:20 -0700

    print comb 1st trial

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - prints 0-9
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+int num;
+for(num = '0'; num <= '9'; num++)
+{putchar(num);
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
