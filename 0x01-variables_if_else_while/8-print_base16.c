337ae28a3f7a0098d888f077811d3bf4b69602e1
jo-nigue <yohaness567@gmail.com>
2022-06-04 11:41:27 -0700

    hexdecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,23 +8,12 @@
  */
 int main(void)
 {
-int num1,num2;
+int num1, num2;
 for (num1 = '0'; num1 <= '9'; num1++)
 putchar(num1);
 for (num2 = 'a'; num2 <= 'f'; num2++)
 putchar(num2);
 putchar('\n');
-/*while(upto9 < 16)
-{
-if (upto9 < 10)
-putchar(upto9);
-else if (upto9 >= 10)
-{
-putchar(above9);
-above9++;
-}
-upto9++;
-}*/
 return (0);
 
 }
